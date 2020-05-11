5b477f4aea1547386906994bbbaaa34b3135e763
Anmol Pant <anmolpant@gmail.com>
2020-05-11 21:55:58 +0530

    added add and delete feature

---
+++ assets/js/todos.js
@@ -0,0 +1,40 @@
+//Check off todos by clicking
+$(document).ready(function() { 
+   $("ul").on("click", "li", function() {
+   	if($(this).css("color") ==="rgb(128, 128, 128)"){
+   		$(this).css("color",'black'); 
+        $(this).css("text-decoration",'none');
+   	}
+
+   	else{
+   		$(this).css("color",'gray'); 
+        $(this).css("text-decoration",'line-through');
+   	}
+        
+  });
+});
+
+//delete
+$(document).ready(function() { 
+   $("ul").on("click", "span", function(event) {
+          $(this).parent().fadeOut(500,function(){
+          	$(this).remove();
+
+
+          });
+          event.stopPropagation();
+   });
+});
+
+//add
+$(document).ready(function() { 
+   $("input[type='text'").keypress(function(event) {
+          if (event.which === 13){
+          	var todotext = ($(this).val());
+          	$(this).val("");
+          	$("ul").append("<li><span>X</span> " + todotext + "</li>")
+          }
+
+
+          });
+ });
